fd21ab62953345991ffc04362383b25a15271a44
devilish <plasticat104@gmail.com>
2021-09-23 17:54:08 +0900

    signin, signup

--- app/src/main/java/com/bokchi/chatting_video/Home.kt
+++ app/src/main/java/com/bokchi/chatting_video/Home.kt
@@ -1,11 +1,37 @@
 package com.bokchi.chatting_video
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Home : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        auth = Firebase.auth
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var id = item?.itemId
+        if (id == R.id.itemLogOut) {
+            Firebase.auth.signOut()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        return true
+    }
+
 }
